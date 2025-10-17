9c8c7ed0e8348d6a204bc2a5fb3c5a4b193dff29
ivbe0111 <berezkinivan94@gmail.com>
2025-10-17 13:33:06 +0200

    added createMemberArrayListFromFile to Member

--- src/Member.java
+++ src/Member.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Member {
 
@@ -7,19 +8,37 @@ public class Member {
     private String email;
     private String personalNumber;
     private LocalDate joinDate;
-    private LocalDate latestSubsriptionPayedDate;
+    private LocalDate latestPayment;
     private MemberType memberType;
+
+    ArrayList<Member> memberList = new ArrayList<>();
+    FileOperations fileOperations = new FileOperations();
 
     public Member(){}
 
-    public Member(String name, String address, String email, String personalNumber, LocalDate joinDate, MemberType memberType) {
+    public Member(String name, String address,
+                  String email, String personalNumber,
+                  LocalDate joinDate, LocalDate latestPayment, MemberType memberType) {
         this.name = name;
         this.address = address;
         this.email = email;
         this.personalNumber = personalNumber;
         this.joinDate = joinDate;
-        this.latestSubsriptionPayedDate = joinDate;
+        this.latestPayment = latestPayment;
         this.memberType = memberType;
+    }
+
+    public ArrayList<Member> createMemberArrayListFromFile(ArrayList<String> memberListUnfiltered){
+        for(String member : memberListUnfiltered){
+            memberList.add(new Member(fileOperations.getNameFromStringArrayList(member),
+                    fileOperations.getAdressFromStringArrayList(member),
+                    fileOperations.getEmailFromStringArrayList(member),
+                    fileOperations.getPersonalnumberFromStringArrayList(member),
+                    fileOperations.getJoinDateFromStringArrayList(member),
+                    fileOperations.getLastPaymentFromStringArrayList(member),
+                    fileOperations.getMemberTypeFromStringArrayList(member)));
+        }
+        return memberList;
     }
 
     public String getName() {
@@ -28,6 +47,54 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPersonalNumber() {
+        return personalNumber;
+    }
+
+    public void setPersonalNumber(String personalNumber) {
+        this.personalNumber = personalNumber;
+    }
+
+    public LocalDate getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(LocalDate joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public LocalDate getLatestPayment() {
+        return latestPayment;
+    }
+
+    public void setLatestPayment(LocalDate latestPayment) {
+        this.latestPayment = latestPayment;
+    }
+
+    public MemberType getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(MemberType memberType) {
+        this.memberType = memberType;
     }
 
     public Member(String name){
